82d4a28da5e6f05021ca318981996fc70c73ee94
Tihomirov-Vasiliy <tihomirov.vasili@bk.ru>
2022-12-29 20:23:34 +0600

    find FileNotFoundException in namespace System.IO and delete from project my custom exception
    add global error handing with ExceptionMiddleware.cs close #4

---
+++ src/WebApi/CustomExceptionMiddleware/ExceptionMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using WebApi.Dtos;
+
+namespace WebApi.CustomExceptionMiddleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+        private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            string errorMessage;
+            context.Response.ContentType = "application/json";
+            switch (exception)
+            {
+                case FileNotFoundException:
+                    context.Response.StatusCode = 404;
+                    errorMessage = exception.Message;
+                    break;
+                default:
+                    context.Response.StatusCode = 500;
+                    errorMessage = "Internal Server Error: something went wrong in data";
+                    break;
+            }
+            await context.Response.WriteAsync(new ErrorDetails(context.Response.StatusCode, errorMessage).ToString());
+        }
+    }
+}
---
+++ src/WebApi/Dtos/ErrorDetails.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+
+namespace WebApi.Dtos
+{
+    public class ErrorDetails
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public ErrorDetails(int statusCode, string message)
+        {
+            StatusCode = statusCode;
+            Message = message;
+        }
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
